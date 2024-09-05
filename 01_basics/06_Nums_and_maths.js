44fd948e6fecae84d43d9617f4b95da3aab573ce
Lalit Hire <lalithireofficial1@gmail.com>
2024-09-05 13:08:43 +0000

    some operations on numbers

--- 01_basics/06_Nums_and_maths.js
+++ 01_basics/06_Nums_and_maths.js
@@ -17,7 +17,7 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
+// console.log(Math.abs(-4));  absolute value
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
